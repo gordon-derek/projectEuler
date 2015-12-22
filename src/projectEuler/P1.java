a875ff58eff9e87551ee4922c3198903585f32a0
Derek Gordon <Derek Gordon>
2015-12-21 23:46:04 -0500

    Initial commit -- Completed problem one

---
+++ src/projectEuler/P1.java
@@ -0,0 +1,25 @@
+// Derek Gordon
+// 12/21/2015
+
+package projectEuler;
+
+public class P1 {
+	private final static int MAX = 1000;
+	
+	public static void main(String[] args){
+		//add all multiples of 3 and 5, subtract the duplicates
+		int multipleSum = sum(3,MAX) + sum(5,MAX) - sum(15,MAX);
+		System.out.println(Integer.toString(multipleSum));
+	}
+	
+	//add num to itself until limit is hit
+	//add on each iteration to sum.
+	private static int sum(int num, int limit){
+		int sum = 0;
+		for(int i = num; i < limit; i+=num){
+			sum += i;
+		}
+		
+		return sum;
+	}
+}
