9eb3df51104014800c159bc82de45ecd2d0421ca
Derek <gordon.derek46@gmail.com>
2015-12-24 16:35:55 -0500

    Added functionality to get number of levels in triangle.  Will be used to make two dimensional array to store triangle in for processing.

--- src/projectEuler/P18.java
+++ src/projectEuler/P18.java
@@ -10,16 +10,26 @@ import java.io.IOException;
  */
 public class P18 {
 
+    private static int getTriDepth(BufferedReader triFile){
+        try {
+            if (triFile.readLine() != null)
+                return 1 + getTriDepth(triFile);
+        }catch(IOException e){e.printStackTrace();}
+        return 0;
+    }
+
     //Pre: filename has been passed into program as argument
     //Post: Filename has been read
     private static int[][] getTriangleFromText(String filename){
         BufferedReader triFile = null;
+        int triangle[][] = null;
+
         try {
             triFile = new BufferedReader(new FileReader(new File(filename)));
-            int lines = 0;
+            int lines = getTriDepth(triFile);
 
-            while (triFile.readLine() != null)
-                lines++;
+            triangle = new int[lines][lines];
+
 
             triFile.close();
 
@@ -28,7 +38,9 @@ public class P18 {
         }finally{
             try {
                 triFile.close();
-            }catch(IOException e){e.printStackTrace();}}
+            }catch(IOException e){e.printStackTrace();}
+        }
+        return triangle;
     }
 
     public static void main(String[] args){
